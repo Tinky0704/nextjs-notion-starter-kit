2b15e7df0a5c98e33b047afb66a9ed98094d1b7e
Tinky0704 <160703872+Tinky0704@users.noreply.github.com>
2024-03-30 15:56:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Tinky-Zhao-6fb1c2cebb144022b85c164c20574320?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Welcome to my virtual zone.',
+  domain: 'https://flash-lilac-774.notion.site/Tinky-Zhao-6fb1c2cebb144022b85c164c20574320?pvs=4',
+  author: 'Tinky Zhao',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
