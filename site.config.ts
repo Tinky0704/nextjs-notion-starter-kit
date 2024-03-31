ded2d845cae375cf00c90474abd4f9b7f62f5f03
Tinky0704 <160703872+Tinky0704@users.noreply.github.com>
2024-03-31 22:56:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -46,7 +46,7 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  // pageUrlOverrides: null,
+  pageUrlOverrides: null,
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
@@ -55,7 +55,7 @@ export default siteConfig({
   navigationLinks: [
      {
        title: 'About',
-       pageId: 'About-b08e10d847b74da2bb6643590294c34e?pvs=4'
+       pageId: 'Resume-Template-3092f8c83a494daaa9da89422cf2b5f0?pvs=4'
      },
      {
        title: 'Contact',
